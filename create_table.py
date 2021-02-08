a5dd1cbb16efc85f3553586f6b9dd1e01e7a85d0
Venkteshwar Tripathi <tripathivenkteshwar@gamil.com>
2021-02-08 17:04:24 +0530

    import os in create_table.py

--- create_table.py
+++ create_table.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =environ.get('DATABASE_URL')
